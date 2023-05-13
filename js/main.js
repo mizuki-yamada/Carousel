2a7df3f5b2915fc31f98c414ce1c9b3f145687fc
mizuki-yamada <my79909369@gmail.com>
2023-05-13 15:20:12 +0900

    最初と最後の画像が表示されるとき、ボタンが消えるように実装

--- js/main.js
+++ js/main.js
@@ -7,14 +7,30 @@
   const slides = ul.children;
   let currentIndex = 0;
 
+  function updateButtons() {
+    next.classList.remove("hidden");
+    prev.classList.remove("hidden");
+
+    if (currentIndex === 0) {
+      prev.classList.add("hidden");
+    }
+    if (currentIndex === slides.length - 1) {
+      next.classList.add("hidden");
+    }
+  }
+    
+    updateButtons()
+
   next.addEventListener("click", () => {
-    currentIndex++;
+      currentIndex++;
+      updateButtons();
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
-    
+
   prev.addEventListener("click", () => {
-    currentIndex--;
+      currentIndex--;
+      updateButtons();
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
